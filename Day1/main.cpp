219b108ca74f776352785cf87656fb6380ca0e3a
Matko Kusenic <mkusenic@incubedit.com>
2020-12-06 22:47:36 +0100

    day 1

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
+#include <stdio.h>
+#include <curl/curl.h>
+#include <fstream>
+#include <string>
+#include <vector>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+
+  std::ifstream file("/home/incubed/AdventOfCode/advent_of_code_2020/Day1/sources/day_1_input.txt");
+  std::vector<int> values;
+
+  for(std::string value; std::getline(file, value);)
+  {
+    values.push_back(stoi(value));
+  }
+
+  file.close();
+  bool found = false;
+
+  for(int i = 0; i < values.size()-1; i++) {
+    for (int j = 0; j < values.size()-1; j++)
+    {
+      if(i == j) continue;
+      if(values[i] + values[j] == 2020)
+      {
+        found = true;
+        std::cout<<"Two values: "<<values[i]<<" + "<<values[j]<<" = "<<values[i]+values[j]<<"| result is "<<values[i]*values[j]<<std::endl;
+      }
+      if (found) break;
+    }
+    if (found) break;
+  }
+
+  found = false;
+
+  for(int i = 0; i < values.size()-1; i++) {
+    for (int j = 0; j < values.size() - 1; j++) {
+      for (int k = 0; k < values.size() - 1; k++) {
+        if (i == j) continue;
+        if (i == k) continue;
+        if (j == k) continue;
+
+        if (values[i] + values[j] + values[k] == 2020) {
+          found = true;
+          std::cout << "Three values: " << values[i] << " + " << values[j] << " + " << values[k] << " = "
+                    << values[i] + values[j] + values[k] << "| result is " << values[i] * values[j] * values[k]
+                    << std::endl;
+        }
+        if (found) break;
+      }
+      if (found) break;
+    }
+    if (found) break;
+  }
   return 0;
 }
